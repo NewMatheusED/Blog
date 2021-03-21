2bf4de2ebdba211cfd239e7c9626a78e5d706fcf
Matheus <matheus081105@gmail.com>
2021-03-21 18:58:59 -0300

    .

--- Blog/app.js
+++ Blog/app.js
@@ -8,4 +8,23 @@ $(function() {
             $('span.button').css('left', '9px')
         }
     })
+
+    var el = $('header')
+
+    curHeight = el.height();
+    autoHeight = el.css('height', 'auto').height();
+
+    $('.navbar-toggler-icon').click(function() {
+        $('body').toggleClass('clicked-nav')
+
+        if($('body').hasClass('clicked-nav')) {
+            $('header').animate({
+                "height": autoHeight
+            },400)
+        }else{
+            $('header').animate({
+                "height": '80px'
+            },400)
+        }
+    })
 })
